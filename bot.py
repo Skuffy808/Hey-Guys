848de2e544d0e6d2c129535053f414561798d553
Skuffy <skuffy808@gmail.com>
2025-05-14 21:28:51 +1200

    Add bot

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
-print("Hello, World!")
+from dotenv import load_dotenv
+import os
+import discord
+
+heyguysList = ["Hey Guys", "hey guys"]
+
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.channel.name == "hey-guys":
+            if message.content in heyguysList:
+                await message.add_reaction('✅')
+            else:
+                await message.add_reaction('❌')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+TOKEN = os.getenv('TOKEN')
+client.run(TOKEN)
